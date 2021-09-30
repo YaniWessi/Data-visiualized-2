dd6565e9aa9fd6b1cc242abbc020387f293f2d48
NCAIN <63156729+nisreencain@users.noreply.github.com>
2021-09-29 20:53:54 -0400

    Add a popup address on the map markers

--- src/content/GeoMap.js
+++ src/content/GeoMap.js
@@ -1,5 +1,5 @@
 import React, {useContext} from "react";
-import {MapContainer, TileLayer, Marker } from 'react-leaflet';
+import {MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import {geoLocationContext} from '../context/geoLocationContext';
 import {storeContext} from "../context/storeContext"
 import ChangeMap from '../components/ChangeMap';
@@ -17,7 +17,12 @@ const GeoMap = () => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <ChangeMap geoData={geoData}/>
-            {stores?.map(store => <Marker key={store?.objectid} position={[store?.latitude, store?.longitude]} />)}
+            {stores?.map(store => <Marker key={store?.objectid} position={[store?.latitude, store?.longitude]}>
+                <Popup>
+                    <strong>{store?.store_name}</strong><br/>
+                    {store?.address}, {store?.city}, {store?.state} {store?.zip5}
+                </Popup>
+            </Marker>)}
         </MapContainer>
     )
 
